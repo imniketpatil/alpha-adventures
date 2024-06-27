691c5ffee217433350374fb4f5f43e2fc0b73472
Niket Patil <patilniket41@gmail.com>
2024-06-27 22:27:53 +0530

    commit and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aa/",
+  base: "/alpha-adventures/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000 kB
