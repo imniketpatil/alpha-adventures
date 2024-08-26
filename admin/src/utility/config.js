c868cc17004b330ac39c2b82a2ce2131105d2076
Niket Patil <patilniket41@gmail.com>
2024-08-26 16:11:23 +0530

    Done Commit in Client

--- admin/src/utility/config.js
+++ admin/src/utility/config.js
@@ -1,4 +1,5 @@
 // config.js
-const client_url = "http://localhost:8000/api/v1";
+const client_url =
+  "https://alpha-adventure-backend-pufvl2eyp-niket-patils-projects.vercel.app";
 
 export default client_url;
