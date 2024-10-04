38b6a21b9ebd346406da5787804b57f361b2ac05
Niket Patil <patilniket41@gmail.com>
2024-10-04 17:46:42 +0530

    Changes in vite.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["axios"], // Add axios or any other external modules you need
-    },
-  },
 });
