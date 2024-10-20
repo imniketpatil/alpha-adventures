a7067d63df06299e629f949de48850fa8b840838
Niket Patil <patilniket41@gmail.com>
2024-10-20 21:26:49 +0530

    Done URL

--- admin/src/utility/config.js
+++ admin/src/utility/config.js
@@ -1,4 +1,4 @@
 // config.js
-const client_url = "https://backend-alpha-adventures.onrender.com/api/v1";
+const client_url = "https://backend-alpha-adventures.onrender.com";
 
 export default client_url;
