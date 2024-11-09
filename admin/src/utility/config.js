5f55455da00522a6c9f90f4fba9bebb853a3baac
Niket Patil <patilniket41@gmail.com>
2024-11-09 12:38:43 +0530

    Done Changes

--- admin/src/utility/config.js
+++ admin/src/utility/config.js
@@ -3,4 +3,6 @@ const client_url = "https://backend-alpha-adventures.onrender.com/api/v1";
 
 // http://localhost:8000/api/v1
 
+//https://backend-alpha-adventures.onrender.com/api/v1
+
 export default client_url;
