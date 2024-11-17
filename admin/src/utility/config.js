891aa214920fa38fafe38d5a9e2eaa3611f1476a
Niket Patil <patilniket41@gmail.com>
2024-11-18 01:25:57 +0530

    Done Changes

--- admin/src/utility/config.js
+++ admin/src/utility/config.js
@@ -4,6 +4,8 @@ const client_url =
 
 // http://localhost:8000/api/v1
 
-//https://backend-alpha-adventures.onrender.com/api/v1
+// https://backend-alpha-adventures.onrender.com/api/v1
+
+// https://backend-alpha-adventures-production.up.railway.app/api/v1
 
 export default client_url;
--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -6,4 +6,5 @@ const client_url =
 
 // https://backend-alpha-adventures.onrender.com/api/v1
 
+// https://backend-alpha-adventures-production.up.railway.app/api/v1
 export default client_url;
